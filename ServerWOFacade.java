d73200c6ca711cfa11ca0b3c4675fbd5abeca44a
Jinjutha <rukkysema@gmail.com>
2021-09-05 20:27:00 +0700

    text

---
+++ ServerWOFacade.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package serverwofacade;
+public class ServerWOFacade {
+    public static void main(String[] args) {
+		MyFacade obj = MyFacade.getMyFacadeObject();
+		System.out.println("Start working......");
+		obj.startServer();
+		System.out.println("After work done.........");
+		obj.destoryServer();
+    }
+}
